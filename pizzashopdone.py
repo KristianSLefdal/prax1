30e2cb6e2f03c429ad91d991d856bc30b6cef38d
KristianSLefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-11-28 15:39:59 +0100

    Add files via upload

---
+++ pizzashopdone.py
@@ -0,0 +1,79 @@
+print ("you own a pizza shop")
+
+ans1 =["Thick"]
+ans2 =["Thin"]
+
+print ("What type of pizza base thick or thin?click enter to show all alternatives and write what you want behind alternatives ")
+
+ans1 = input("Thick")
+if ans1:
+    print ("Thick slice please")
+    print ("The customer wants a thick slice")
+
+ans2 = input("Thin")
+if ans2:
+    print ("Thin slice please")
+    print ("The customer wants a thin slice")
+
+print ("pizza size small (30kr) medium (40kr) large (50kr) enter space 3 times to get all sizes select by type number x behind")
+s1 =["small","1"]
+s2 =["medium", "2"]
+s3 =["large", "3"]
+
+print ("What pizza size? small medium or large")
+s1 = input("1")
+if s1: 
+    print ("small size please")
+s2 = input("2")
+if s2:
+    print ("medium size please")
+s3 = input("3")
+if s3:
+    print ("large pizza size")
+
+print ("What toppings? write number x behind the following number to add")
+print ("cheese 5kr","mushrooms 3kr", "avocado 7kr ", "pineapple 5kr", "bacon 7kr", "chicken 7kr")
+t1 = ["chesse", "5kr"]
+t2 = ["mushrooms", "3kr"]
+t3 = ["avocado 7kr"]
+t4 = ["pineapple 5kr"]
+t5 = ["bacon 7kr"]
+t6 = ["chicken 7kr"]
+
+t1 = input("1")
+if t1:
+    print ("cheese")
+t2 = input("2")
+if t2:
+    print ("mushrooms")
+t3 = input("3")
+if t3:
+    print ("avocado")
+t4 = input("4")
+if t4:
+    print ("pineapple")
+t5 = input("5")
+if t5:
+    print ("bacon")
+t6 = input("6")
+if t6:
+    print ("chicken")
+print ("extra toppings?add number x behind the following number to add")
+t1 = input("1")
+if t1:
+    print ("cheese")
+t2 = input("2")
+if t2:
+    print ("mushrooms")
+t3 = input("3")
+if t3:
+    print ("avocado")
+t4 = input("4")
+if t4:
+    print ("pineapple")
+t5 = input("5")
+if t5:
+    print ("bacon")
+t6 = input("6")
+if t6:
+    print ("chicken")
