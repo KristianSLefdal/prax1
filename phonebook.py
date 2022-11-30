0199a4182a777ba4c4c8ff65f1df240bad60ab46
KristianSLefdal <119334077+KristianSLefdal@users.noreply.github.com>
2022-11-30 12:33:33 +0100

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,10 +1,4 @@
 phonebook = {'Noroff': '38 00 00 00' ,'Playstation': '56 99 93 10' , 'Apple': '23 00 00 00' , 'IBM':'49 00 00 00', 'DELL':'99 00 00 00'}
-print(phonebook['Apple'])
-
-new_contact = input('Apple:')
-new_pn = input('23 00 00 00')
-
-phonebook[new_com] = new_pn
 print(phonebook)
 
 search_dict = input('company reasearch: ')
